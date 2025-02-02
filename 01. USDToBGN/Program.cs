4ad292544ae35130cbd1ea7d56b9dad04fa3f7e7
Tsvetelin Marinov <Cvetelinmarinov566@gmail.com>
2025-02-02 18:29:46 +0200

    Exercise 1 - USD to BGN converter

---
+++ 01. USDToBGN/Program.cs
@@ -0,0 +1,13 @@
+﻿/**
+ * 01. USD to BGN
+ */
+
+
+//1 USD = 1.79549 BGN.
+
+
+double dollars = double.Parse(Console.ReadLine());
+
+double leva = dollars * 1.79549;
+
+Console.WriteLine($"{leva}");
